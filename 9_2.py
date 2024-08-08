054d5fa80dd9fba105750cd4e569b6d48ec0e75f
jammmin02 <jammmin02@naver.com>
2024-08-08 17:50:53 +0900

    country

---
+++ 9_2.py
@@ -0,0 +1,26 @@
+# 새 딕셔너리를 travel_log에 넣는 함수 작성
+def new_countries(counrty_name, visits_num, citi_name):
+    new_country = {
+        
+    }
+    
+
+
+country = input() # 나라 이름
+visits = int(input()) # 방문 횟수
+list_of_cities = eval(input()) # 방문한 도시 
+
+# 기본 리스트
+travel_log = [
+  {
+    "country": "France",
+    "visits": 12,
+    "cities": ["Paris", "Lille", "Dijon"]
+  },
+  {
+    "country": "Germany",
+    "visits": 5,
+    "cities": ["Berlin", "Hamburg", "Stuttgart"]
+  },
+]
+
